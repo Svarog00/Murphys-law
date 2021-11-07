1d852da8d9cd710f470d8aa0148bc299e4ae92e9
Anton Verkhovets <54817442+Svarog00@users.noreply.github.com>
2021-11-07 12:35:04 +0300

    I m tired boss

--- MurphysLaw/Assets/Scripts/Obstacles/ObstacleController.cs
+++ MurphysLaw/Assets/Scripts/Obstacles/ObstacleController.cs
@@ -7,9 +7,12 @@ public class ObstacleController : MonoBehaviour
     private ObstacleModel _obstacleModel;
     private Player _player;
     private Animator _animator;
+    private AudioSource _audioSource;
+    private float _distance;
 
     [SerializeField] private ToolTypes _neededTool;
-    [SerializeField] private UI_DeathScreen _DeathScreen;
+    [SerializeField] private UI_DeathScreen _deathScreen;
+    [SerializeField] private string _deactivationSoundName;
 
     public ObstacleModel Model => _obstacleModel;
 
@@ -19,6 +22,23 @@ public class ObstacleController : MonoBehaviour
         _player = FindObjectOfType<Player>();
         _obstacleModel = new ObstacleModel(_neededTool);
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        _deathScreen.OnCharDeath += OnCharDeath;
+    }
+
+    private void OnCharDeath()
+    {
+        StopSound();
+    }
+
+    private void Update()
+    {
+        _distance = EstimateDistance();
+        _audioSource.volume = 1 / (_distance * 10);
     }
 
     private void OnMouseUp()
@@ -26,6 +46,8 @@ public class ObstacleController : MonoBehaviour
         if (_neededTool == _player.CurTool.Type)
         {
             _obstacleModel.Deactivate();
+            StopSound();
+            FindObjectOfType<AudioManager>().Play(_deactivationSoundName);
         }
     }
 
@@ -41,6 +63,18 @@ public class ObstacleController : MonoBehaviour
 
     private void ActivateDeathScreen()
     {
-        _DeathScreen.Activate();
+        _deathScreen.Activate();
+    }
+
+    private float EstimateDistance()
+    {
+        Vector2 vectorToPlayer = transform.position - _player.gameObject.transform.position; //направленный вектор к игроку / a vector to the player
+        float distanceToPlayer = vectorToPlayer.magnitude; //длина вектора / lenght of the vector
+        return distanceToPlayer;
+    }
+
+    private void StopSound()
+    {
+        _audioSource.Pause();
     }
 }
--- MurphysLaw/Assets/Scripts/UI/UI_DeathScreen.cs
+++ MurphysLaw/Assets/Scripts/UI/UI_DeathScreen.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class UI_DeathScreen : MonoBehaviour
 {
+    public event Action OnCharDeath;
+
     [SerializeField] private SpriteRenderer _sprite;
     [SerializeField] private GameObject _visual;
     [SerializeField] private GameObject _toolSetUI;
@@ -18,6 +21,7 @@ public class UI_DeathScreen : MonoBehaviour
     {
         _visual.SetActive(true);
         _toolSetUI.SetActive(false);
+        OnCharDeath?.Invoke();
     }
 
     public void Exit()
@@ -27,6 +31,6 @@ public class UI_DeathScreen : MonoBehaviour
 
     public void Again()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 }
